3b871f90ca485e76c5589953724a84c8f3d68430
Thaneesh Babu <gkthaneesh2@gmail.com>
2022-09-21 23:26:46 -0400

    established mongo conn

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
-const express = require("express");
+dotenv.config();
 const app = express();
-const cors = require("cors");
-
 app.use(express.json());
-app.use(cors({ credentials: true, origin: `http://localhost:${process.env.CLIENT_PORT}` }));
+app.use(
+  cors({
+    credentials: true,
+    origin: `http://localhost:${process.env.CLIENT_PORT}`,
+  })
+);
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => {
+    throw err;
+  });
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`running on port ${process.env.SERVER_PORT}`);
+  console.log(`running on port ${process.env.SERVER_PORT}`);
 });
