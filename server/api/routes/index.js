8a4da47038804e358de0ad77379f0088e13b9ada
nmhossain02 <nmhossain02@gmail.com>
2023-01-02 15:11:36 -0600

    Python example route created

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -12,3 +12,16 @@ defaultRouter.use("/events", eventRoutes);
 defaultRouter.use("/courses", courseRoutes);
 defaultRouter.use("/calendars", calendarRoutes);
 
+import { spawn } from "child_process";
+
+// https://stackoverflow.com/questions/23450534/how-to-call-a-python-function-from-node-js
+// for debugging: https://stackoverflow.com/questions/4482686/check-synchronously-if-file-directory-exists-in-node-js
+defaultRouter.get("/python", (req, res) => {
+    console.log("route hit")
+    const path = 'nlp/hello.py'
+    const pyScript = spawn('python', [path])
+    pyScript.stdout.on('data', data => {
+        console.log(data.toString());
+        res.json({ message: data }).status(200);
+    });
+})
