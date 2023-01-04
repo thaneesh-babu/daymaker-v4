56861ce507703d213dba6e8d7e685437bded7181
nmhossain02 <nmhossain02@gmail.com>
2023-01-04 14:38:05 -0600

    Merge cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import FileUpload from './components/FileUpload'
-import EditEvents from './components/EditEvents/EditEvents'
-import { Box } from "@chakra-ui/react"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import FileUpload from "./components/FileUpload";
+import EditEvents from "./components/EditEvents/EditEvents";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return (
-    <BrowserRouter>
+    return (
         <Routes>
-          <Route path='/' element={<FileUpload/>} />
-          <Route path='/edit' element={ <EditEvents /> } />
-      </Routes>
-    </BrowserRouter>
-  )
+            <Route path="/" element={<FileUpload />} />
+            <Route path="/edit" element={<EditEvents />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
